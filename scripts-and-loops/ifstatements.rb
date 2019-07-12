244eb208a0b3ae3612878587604e5abd0ce8485a
Sachin Verma <sachin61900@gmail.com>
2019-07-12 16:14:40 -0400

    Add files via upload

---
+++ scripts-and-loops/ifstatements.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby
+
+#Basic If Statements
+
+# x = 4
+
+# if x == 4
+# 	puts "x is 4"
+# end
+
+# x = 12
+
+# if x == 12
+
+# 	puts "x is 12"
+
+# elsif x == 27
+
+# 	puts "x is 27"
+
+# end
+
+name = "Vishal"
+
+if name == "Sunil"
+	puts "Dad"
+elsif name == "Sabina"
+	puts "Mom"
+elsif name == "Shafali"
+	puts "Sister"
+elsif name == "Rishi"
+	puts "Brother"
+elsif name == "Riana"
+	puts "Sister"
+else
+	puts "Not living in my house"
+end
---
+++ scripts-and-loops/loops.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+
+# arr = [1,2,3,4]
+
+# arr.each do |a| 
+# 	puts a
+# end
+
+family = ["Sunil", "Sabina", "Shafali", "Rishi", "Riana"]		
+
+# family.each do |name|
+# 	puts name
+# end
+
+family.each do |name|
+	if name == "Rishi"
+		puts "Rishi is the Best"
+	end
+end
